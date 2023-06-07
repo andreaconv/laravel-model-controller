6fed1222ed72b471d41a19e1dd667863f0bdb742
andreaconv <andrea2convertini@gmail.com>
2023-06-07 17:20:04 +0200

    [add] table per visualizzare i dettagli dei film

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,41 +5,32 @@
   <div class="home">
     <div class="container py-5">
 
-      @foreach ($movies as $movie)
+      {{-- @dd($movies) --}}
 
-        <h3>{{ $movie->title }}</h3>
-
-      @endforeach
-
-      {{-- <table class="table table-striped">
+      <table class="table table-striped">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Nazionalità</th>
+            <th scope="col">Data di uscita</th>
+            <th scope="col">Voto</th>
+            <th scope="col">Azioni</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          @foreach ($movies as $movie)
+            <tr>
+              <th scope="row">{{ $movie->id }}</th>
+              <td>{{ $movie->title }}</td>
+              <td>{{ $movie->nationality }}</td>
+              <td>{{ $movie->date }}</td>
+              <td>{{ $movie->vote }}</td>
+              <td><a href="#" class="btn btn-success">Vai</a></td>
+            </tr>
+          @endforeach
         </tbody>
-      </table> --}}
+      </table>
     </div>
   </div>
 
